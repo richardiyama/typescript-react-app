5ccaab38c60c3cfd4706ca59f42d52efffce057a
Richard Iyama <richardiyama@yahoo.com>
2020-10-02 12:47:37 +0100

    Added Implementation for Context Api,Reducer,Cart and Subtotal

--- src/store/counter/CounterReducer.ts
+++ src/store/counter/CounterReducer.ts
@@ -1,10 +1,10 @@
 import { Reducer } from 'react';
 import {CounterActionTypes, INCREMENT_COUNTER, DECREMENT_COUNTER} from './models/actions';
 
-import {Counter} from './models/Counter';
+import {Counter} from './models/counter';
 
 export const counterReducer: Reducer<Counter,Action> = (state,action) => {
-    
+
 }
 
 
